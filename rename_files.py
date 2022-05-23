5aabbb3c3645935d016857efe0c7f7ed2c92d01a
Anıl Savaşkurt <46941493+anilsavaskurt@users.noreply.github.com>
2022-05-23 22:59:44 +0300

    initial commit

---
+++ rename_files.py
@@ -0,0 +1,16 @@
+import os
+
+# write path where is your folder that includes files
+path = "C:/Users/..."
+
+liste=os.listdir(path)
+
+# you can use x variable to rename folders like newfile0.png to newfilex.png
+# also you can change png as ".pdf , .jpeg, .jpg" etc. 
+
+x = 0
+for i in liste:
+    print(path+i)
+    os.rename(path + i ,path + "newName" + str(x) + ".png")
+    x = x+1
+    print(x)
